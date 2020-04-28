b189ca4c5f4c6d329c8ae4d0dce6ea1d190913a8
mohamedgit config --global user.namemohamed-gamal <mohamed gamal>
2020-04-28 04:44:22 +0200

    Improve progressed

--- Flash Chat/LogInViewController.swift
+++ Flash Chat/LogInViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import SVProgressHUD
 
 class LogInViewController: UIViewController {
 
@@ -26,7 +27,8 @@ class LogInViewController: UIViewController {
    
     @IBAction func logInPressed(_ sender: AnyObject)
     {
-
+        
+        SVProgressHUD.show()
         
         //TODO: Log in the user
         Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!)
@@ -38,6 +40,7 @@ class LogInViewController: UIViewController {
             else
             {
                 print("Login Sucess")
+                SVProgressHUD.dismiss()
             }
             self.performSegue(withIdentifier: "goToChat", sender: self)
         }
--- Flash Chat/RegisterViewController.swift
+++ Flash Chat/RegisterViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import SVProgressHUD
 
 class RegisterViewController: UIViewController {
 
@@ -31,6 +32,7 @@ class RegisterViewController: UIViewController {
   
     @IBAction func registerPressed(_ sender: AnyObject)
     {
+        SVProgressHUD.show()
         //TODO: Set up a new user on our Firbase database
         Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!)
         { (user, error) in
@@ -42,7 +44,7 @@ class RegisterViewController: UIViewController {
         else
         {
             print("Registeration Sucess")
-            
+            SVProgressHUD.dismiss()
             self.performSegue(withIdentifier: "goToChat", sender: self)
         }
         
