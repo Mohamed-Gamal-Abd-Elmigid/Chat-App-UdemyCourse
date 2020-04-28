6999dbf387ab93fe399e156839317f399f2e7ce2
mohamedgit config --global user.namemohamed-gamal <mohamed gamal>
2020-04-28 05:00:53 +0200

    implement the ChamelCaseFramwork

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import ChameleonFramework
 
 class ChatViewController: UIViewController , UITableViewDelegate , UITableViewDataSource  , UITextFieldDelegate
 {
@@ -48,6 +49,8 @@ class ChatViewController: UIViewController , UITableViewDelegate , UITableViewDa
         messageTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
             configureTableView()
             retrieveMessages()
+        
+        messageTableView.separatorStyle = .none
     }
 
     ///////////////////////////////////////////
@@ -65,6 +68,18 @@ class ChatViewController: UIViewController , UITableViewDelegate , UITableViewDa
         cell.messageBody.text = messageArray[indexPath.row].messageBody
         cell.senderUsername.text = messageArray[indexPath.row].sender
         cell.avatarImageView.image = UIImage(named: "egg")
+        
+        if cell.senderUsername.text == Auth.auth().currentUser?.email as String?
+        {
+            cell.avatarImageView.backgroundColor = UIColor.flatMint()
+            cell.messageBackground.backgroundColor = UIColor.flatSkyBlue()
+        }
+        else
+        {
+            cell.avatarImageView.backgroundColor = UIColor.flatWatermelon()
+            cell.messageBackground.backgroundColor = UIColor.flatGray()
+        }
+        
         return cell
     }
     
